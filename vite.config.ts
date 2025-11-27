37625c55adbe6fd5624d3a1ac8fc6251187188f8
leokapsy <leokapfy@gmail.com>
2025-11-27 09:39:56 +0200

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-repo/",
+  base: "/hotels-demo/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
